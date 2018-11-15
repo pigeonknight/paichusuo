d8893d47d466b42a1f7f185182815765402a41c6
BestKang <34906546+BestKang@users.noreply.github.com>
2018-11-15 13:56:55 +0800

    对对对

--- src/main/java/com/police/controller/copstaffmanagement/CopStaffInfoController.java
+++ src/main/java/com/police/controller/copstaffmanagement/CopStaffInfoController.java
@@ -1,5 +1,22 @@
 package com.police.controller.copstaffmanagement;
 
+@Controller
+@RequestMapping("copstaffmanagement")
 public class CopStaffInfoController {
+    private static final Logger logger = LoggerFactory.getLogger(CopStaffInfoController.class);
+    @Autowired
+    private CopStaffInfoService copStaffInfoService;
+    @ResponseBody
+    @RequestMapping(value = "/createmaintask", method = RequestMethod.POST)
+    public String createMainTaskInfo(@RequestBody String payload) {
+        logger.info("获取主任务列表， 请求参数：{}", payload);
+        TaskInfoPO taskInfo = FastJsonUtil.toBean(payload, TaskInfoPO.class);
+        Integer resultColumn = mainTaskInfoService.createMainTaskInfo(taskInfo);
+        if (resultColumn != null) {
+            return ResultBuilder.buildSuccess("创建主任务成功");
+        } else {
+            return ResultBuilder.buildError("创建主任务失败");
+        }
+    }
 
 }
